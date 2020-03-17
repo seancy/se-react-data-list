5a8f93976d03f3353fadcf234f7992821639b86c
Sean <se.zhang@learning-tribes.com>
2020-03-17 09:55:44 +0800

    Dynamic generate fields Remove comments.

--- src/index.js
+++ src/index.js
@@ -3,74 +3,52 @@ import {render} from "react-dom";
 import Component from "./component";
 import './index.scss'
 import $ from "jquery";
-//import './i18n'
 
 const styles = {
     fontFamily: "sans-serif",
 };
-//Name	Email	Country	Commercial Zone	Commercial Region	City	Locatio
 
-/*
-	        edx@example.com	—	—	—	—	—	—	1	0	0	1	0	0%	0 / 4	0	0:00:00	04/06/2019
-The Honor	honor@example.com	—	—	—	—	—	—	1	0	0	1	0	0%	0 / 4	0	0:00:00	23/12/2019
-Judy Sim	audit@example.com	—	—	—	—	—	—	1	0	0	1	0	0%	0 / 4	0	0:00:00	26/12/2019
-verified	verified@example.com	—	—	—	—	—	—	1	0	0	1	0	0%	0 / 4	0	0:00:00	23/12/2019
-Geo Marie  	staff@example.com	—	—	—	—	—	—	1	0	0	1	0	0%	0 / 4	35	16:45:26	05/02/2020
-Jale Dave	davejale@gmail.com	France	—	—	shanghai	—	2345678	1	0	0	0	1	0%	0 / 4	0	0:00:00	—
-Jale Dave1	davejale1@gmail.com
-*/
 const numRender={
     render:v=>`--${v}--`
 }
 
-let groups = [
-    { name:'names', fieldNames:['first_name', 'last_name']},
-]
-let fields = [
-    //{ name:'', fieldName:'id' },
-    /*{name: 'Name', fieldName: 'userName'},
-    {name: 'Email', fieldName: 'email'},
-    {name: 'Country', fieldName: 'country'},*/
-    //{ name:'', fieldName:'id' },
-    {name: 'email', fieldName: 'email'},
-    {name: 'first_name', fieldName: 'first_name'},
-    {name: 'last_name', fieldName: 'last_name'},
-    {name: 'num', fieldName: 'num', ...numRender},
-    {name: 'avatar', fieldName: 'avatar'},
-    //{ name:'first_name1', fieldName:'first_name' },
-    /*{ name:'last_name1', fieldName:'last_name' },
-    { name:'first_name2', fieldName:'first_name' },
-    { name:'last_name2', fieldName:'last_name' },
-    { name:'first_name3', fieldName:'first_name' },
-    { name:'last_name3', fieldName:'last_name' },
-    { name:'first_name4', fieldName:'first_name' },
-    { name:'last_name4', fieldName:'last_name' },
-    { name:'first_name5', fieldName:'first_name' },
-    { name:'last_name5', fieldName:'last_name' },*/
-]
-let data_p1 = [
-    {id: 'a0', test1: 't1', userName: 'The Honor', email: 'honor@example.com', country: 'US'},
-]
-let data2 = [
-    {id: 'a0', test1: 't1', userName: 'The Honor', email: 'honor@example.com', country: 'US'},
-    {id: 'a1', test1: 't11', userName: 'Judy Sim', email: 'audit@example.com', country: 'France'},
-    {id: 'a2', test1: 't1111', userName: 'verified', email: 'verified@example.com', country: 'Brazil'},
-]
-let totalData = {
-    email: '3'
+const extraProps = {
+    cellRender: v=>{
+        if ((v.startsWith('Yes') || v.startsWith('No')) && v.includes(':')){
+            const arr = v.split(':')
+            return `${arr[0]=='Yes'?'Correct':'Wrong'} ${arr[1]}%` //
+        }else{
+            return v
+        }
+    }
 }
 
-//https://reqres.in/api/users?page=2
+let fields = [
+    {name: 'Name', fieldName: 'Name'},
+    {name: 'City', fieldName: 'City'},
+    {name: 'Email', fieldName: 'Email'},
+]
 
-class App extends React.Component {
+
+
+class BaseReport extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data: [],
+            totalData: {},
+            rowsCount: 0
+        }
+    }
+}
+
+class App extends BaseReport {
     constructor(props, context) {
         super(props, context);
 
         //fetch data then place data and rowsCount into state
         this.state = {
-            data: [],
-            totalData: {},
-            rowsCount: 0
+            ...this.state,
         }
 
         this.myRef = React.createRef();
@@ -87,7 +65,8 @@ class App extends React.Component {
         let data = {a1: '11', a2: '22'}
         let parametersStr = Object.keys(data).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(data[k])}`).join('&')
 
-        const url = `data.json`
+        //const url = `real_data.json`
+        const url = 'course_report_time_spent.json'
         let toolbarData = {
             selectedFilterItems: [
                 {text: "Address", value: "De street"},
@@ -132,16 +111,60 @@ class App extends React.Component {
 
     }
 
+    getDynamicFields(){
+        const {data} = this.state
+        const propertiesValues = fields.map(f=>f.fieldName)
+
+        let dynamicFields = [], subFields = []
+        if (data && data.length > 0){
+            const firstRow = data[0]
+            const dynamicKeys = Object.keys(firstRow)
+                .filter(key=>{
+                    return !propertiesValues.includes(key) // && !key.split('/')[1]
+                })
+            const complexDynamicKeys = dynamicKeys.filter(key=>key.split('/')[1])
+            const complexDynamicKeysL1 = complexDynamicKeys.map(key=>key.split('/')[0])
+            //const complexDynamicKeysL2 = complexDynamicKeys.map(key=>key.split('/')[1])
+
+            const normalDynamicFields = dynamicKeys.filter(key=>!key.split('/')[1])
+            const complexDynamicFields = [...new Set(complexDynamicKeys.map(key=>key.split('/')[0]))]
+            const countSpan = (key)=>{
+                return complexDynamicKeysL1.filter(l1key=>key==l1key).length
+            }
+
+            dynamicFields = [...normalDynamicFields.map(key=>({name:key,fieldName:key})),
+                ...complexDynamicFields.map(key=>({name:key,fieldName:key, colSpan:countSpan(key)}))]
+            subFields = complexDynamicKeys.map(key=>{
+                const arr = key.split('/')
+                const keyL2 = arr[1]
+                return {name:keyL2,fieldName:key}
+            })
+        }
+        return {dynamicFields, subFields}
+    }
+
+    getConfig(){
+        const {dynamicFields, subFields}=this.getDynamicFields()
+        return {
+            fields:fields.map(item=>({...item})).concat(dynamicFields),
+            subFields,
+            pagination:{
+                pageSize: 2, rowsCount: Number(this.state.rowsCount)
+            },
+            keyField:"ID",
+            defaultLanguage:"en",
+            enableRowsCount:true,
+            ...this.state
+        }
+    }
 
     render() {
-        const pagination = {pageSize: 2, rowsCount: this.state.rowsCount}
+        const config = this.getConfig()
         return (
             <div style={styles}>
                 <button className="btn-load-data" onClick={this.loadData.bind(this)}>Load data</button>
-                <Component ref={this.myRef} enableRowsCount={true}
-                           defaultLanguage="en"
-                           keyField="id" {...this.state}
-                           {...{fields}} pagination={pagination}
+                <Component ref={this.myRef}
+                           {...config} {...extraProps}
                            onPageChange={this.fetchData.bind(this)}
                 />
             </div>
@@ -151,45 +174,3 @@ class App extends React.Component {
 
 render(<App/>, document.querySelector(".root"));
 
-
-
-
-    /*
-    let data = {a1:'11', a2:'22'}
-    let parametersStr = Object.keys(data).map(k=>`${encodeURIComponent(k)}=${encodeURIComponent(data[k])}`).join('&')
-    */
-
-
-        /*fetch(url).then(response => {
-            return response.json()
-        }).then(json => {
-            this.setState((s, p) => {
-                return {
-                    data: json.data,
-                    totalData: {email: 'total:', first_name: json.total},
-                    rowsCount: json.total
-                }
-            })
-        })*/
-
-    /*fetchData(pageNo) {
-        /!*page: 1
-        per_page: 6
-        total: 12
-        total_pages: 2*!/
-        let data = {a1: '11', a2: '22'}
-        let parametersStr = Object.keys(data).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(data[k])}`).join('&')
-
-        const url = `https://reqres.in/api/users?page=${pageNo}&per_page=2${(parametersStr ? ('&' + parametersStr) : '')}`
-        fetch(url).then(response => {
-            return response.json()
-        }).then(json => {
-            this.setState((s, p) => {
-                return {
-                    data: json.data,
-                    totalData: {email: 'total:', first_name: json.total},
-                    rowsCount: json.total
-                }
-            })
-        })
-    }*/
